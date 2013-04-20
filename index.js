b1fad71906a0f97569323e3667c98e50a655bc53
Carter Thaxton <carter.thaxton@gmail.com>
2013-04-19 17:42:16 -0700

    Working edge detection and image loading

--- index.js
+++ index.js
@@ -2,21 +2,35 @@ var ffi = require('ffi');
 var ref = require('ref');
 var Struct = require('ref-struct');
 
+
 // ---- Types ----
-var VoidPtr = ref.refType(ref.types.void);
-var CvCapturePtr = VoidPtr;
-var CvArrPtr = VoidPtr;
+function ptr(type) {
+  return ref.refType(type);
+}
+
+var Void = ref.types.void;
+var VoidPtr = ptr(Void);
 
 var CvSize = Struct({
   width: 'int',
   height: 'int'
 });
 
-var IplImage = Struct({
-
+var CvPoint = Struct({
+  x: 'int',
+  y: 'int'
 });
-var IplImagePtr = ref.refType(IplImage);
-var IplImagePtrPtr = ref.refType(IplImagePtr);
+
+var IplImage = VoidPtr;
+var IplImagePtr = ptr(IplImage);
+var IplImagePtrPtr = ptr(IplImagePtr);
+
+var Mat = VoidPtr;
+var MatPtr = ptr(Mat);
+var MatPtrPtr = ptr(MatPtr);
+
+var CvCapturePtr = VoidPtr;
+var CvArrPtr = VoidPtr;
 
 
 // ---- Functions ----
@@ -24,10 +38,13 @@ var core = ffi.Library('libopencv_core', {
   'cvGetSize': [ CvSize, [ CvArrPtr ] ],
   'cvCreateImage': [ IplImagePtr, [ CvSize, 'int', 'int' ] ],
   'cvReleaseImage': [ 'void', [ IplImagePtrPtr ] ],
+  'cvCreateMat': [ MatPtr, [ 'int', 'int', 'int' ] ],
+  'cvReleaseMat': [ 'void', [ MatPtrPtr ] ],
 });
 
 var imgproc = ffi.Library('libopencv_imgproc', {
   'cvCvtColor': [ 'void', [ CvArrPtr, CvArrPtr, 'int' ] ],
+  'cvCanny': [ 'void', [ CvArrPtr, CvArrPtr, 'double', 'double', 'int' ] ],
 });
 
 var highgui = ffi.Library('libopencv_highgui', {
@@ -35,7 +52,8 @@ var highgui = ffi.Library('libopencv_highgui', {
   'cvQueryFrame': [ IplImagePtr, [ CvCapturePtr ] ],
   'cvGrabFrame': [ 'bool', [ CvCapturePtr ] ],
   'cvRetrieveFrame': [ IplImagePtr, [ CvCapturePtr ] ],
-  'cvSaveImage': [ 'int', [ 'string', IplImagePtr ] ],
+  'cvLoadImage': [ IplImagePtr, [ 'string', 'int' ] ],
+  'cvSaveImage': [ 'int', [ 'string', IplImagePtr, VoidPtr ] ],
 });
 
 
@@ -45,21 +63,41 @@ var cv = {};
 cv.getSize = core.cvGetSize;
 cv.createImage = core.cvCreateImage;
 cv.releaseImage = core.cvReleaseImage;
+cv.createMat = core.cvCreateMat;
+cv.releaseMat = core.cvReleaseMat;
 
 cv.cvtColor = imgproc.cvCvtColor;
+cv.canny = imgproc.cvCanny;
 
 cv.captureFromCAM = highgui.cvCreateCameraCapture;
 cv.queryFrame = highgui.cvQueryFrame;
 cv.grabFrame = highgui.cvGrabFrame;
 cv.retrieveFrame = highgui.cvRetrieveFrame;
+cv.loadImage = highgui.cvLoadImage;
 cv.saveImage = highgui.cvSaveImage;
 
 
 // ---- Enums ----
+cv.BGR2BGRA    = 0;
+cv.RGB2BGRA    = cv.BGR2BGRA;
+cv.BGRA2BGR    = 1;
+cv.RGBA2RGB    = cv.BGRA2BGR;
+cv.BGR2RGBA    = 2;
+cv.RGBA2RGB    = cv.BGR2RGBA;
+cv.RGBA2BGR    = 3;
+cv.BGRA2RGB    = cv.RGBA2BGR;
+cv.BGR2RGB     = 4;
+cv.RGB2BGR     = cv.BGR2RGB;
+cv.BGRA2RGBA   = 5;
+cv.RGBA2BGRA   = cv.BGRA2RGBA;
 cv.BGR2GRAY    = 6;
 cv.RGB2GRAY    = 7;
 cv.GRAY2BGR    = 8;
 cv.GRAY2RGB    = cv.GRAY2BGR;
+cv.GRAY2BGRA   = 9;
+cv.GRAY2RGBA   = cv.GRAY2BGRA;
+cv.BGRA2GRAY   = 10;
+cv.RGBA2GRAY   = 11;
 
 
 module.exports = cv;
--- test/test.js
+++ test/test.js
@@ -18,33 +18,36 @@ function endSync(name) {
   end(name, name + ' - sync')
 }
 
-// can't run this async on mac, due to event loop
-var camera = cv.captureFromCAM(-1);
-
-start('processing');
-
-cv.queryFrame.async(camera, function(err, image) {
-
-  var size = cv.getSize(image);
-
-  start('create image')
-  cv.createImage.async(size, 8, 1, function(err, image_gray) {
-    end('create image')
-
-    start('convert grayscale')
-    cv.cvtColor.async(image, image_gray, cv.BGR2GRAY, function(err) {
-      end('convert grayscale')
-
-      cv.saveImage.async('out/orig.jpg', image, function(err, result) {
-        cv.releaseImage(image.ref())
-        cv.saveImage.async('out/gray.jpg', image_gray, function(err, result) {
-          cv.releaseImage(image_gray.ref())
-
-          end('processing');
-        })
-      })
-    })
-    endSync('convert grayscale')
+function outputAndRelease(filename, image, cb) {
+  cv.saveImage.async(filename, image, null, function(err, result) {
+    cv.releaseImage(image.ref())
+    if (cb) cb()
   })
-  endSync('create image')
+}
+
+// can't run this async on mac, due to event loop
+//var camera = cv.captureFromCAM(-1)
+//cv.queryFrame.async(camera, function(err, img_orig) {
+
+cv.loadImage.async('data/input.jpg', 1, function(err, img_orig) {
+
+  console.log('starting')
+
+  var size = cv.getSize(img_orig)
+  var img_gray = cv.createImage(size, 8, 1)
+  var img_edges = cv.createImage(size, 8, 1)
+
+  cv.cvtColor.async(img_orig, img_gray, cv.BGR2GRAY, function(err) {
+    cv.canny.async(img_gray, img_edges, 5, 300, 3, function(err) {
+
+      outputAndRelease('data/orig.jpg', img_orig, function() {
+        outputAndRelease('data/gray.jpg', img_gray, function() {
+          outputAndRelease('data/edges.jpg', img_edges, function() {
+            console.log('done');
+          });
+        });
+     });
+
+    })
+  })
 })
